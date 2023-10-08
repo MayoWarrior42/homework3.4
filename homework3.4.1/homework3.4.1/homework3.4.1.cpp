ba2d0ff6f50420e1fc305f4cfad049a9e32065af
MayoWarrior <sweetyl4255.55@gmail.com>
2023-10-08 12:13:19 +0300

    homework3.4

---
+++ homework3.4.1/homework3.4.1/homework3.4.1.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+class Address {
+private:
+    std::string city;
+    std::string street;
+    int houseNumber;
+    int apartmentNumber;
+public:
+    Address(std::string city, std::string street, int houseNumber, int apartmentNumber) {
+        this->city = city;
+        this->street = street;
+        this->houseNumber = houseNumber;
+        this->apartmentNumber = apartmentNumber;
+    }
+
+    std::string getOutputAddress() {
+        return city + ", " + street + ", " + std::to_string(houseNumber) + ", " + std::to_string(apartmentNumber);
+    }
+};
+
+int main() {
+    int n;
+    std::ifstream fin("in.txt");
+    std::ofstream fout("out.txt");
+
+    if (fin.is_open()) {
+       fin >> n;
+        Address** addresses = new Address * [n];
+
+        for (int i = 0; i < n; i++) {
+            std::string city, street;
+            int houseNumber, apartmentNumber;
+            fin.ignore(256, '\n');
+            std::getline(fin, city);
+            std::getline(fin, street);
+            fin >> houseNumber;
+            fin >> apartmentNumber;
+
+            addresses[i] = new Address(city, street, houseNumber, apartmentNumber);
+        }
+
+        fin.close();
+
+       fout << n << std::endl;
+
+        for (int i = n - 1; i >= 0; i--) {
+            std::string outputAddress = addresses[i]->getOutputAddress();
+            fout << outputAddress << std::endl;
+
+            delete addresses[i];
+        }
+
+        delete[] addresses;
+
+        fout.close();
+    }
+    else {
+        std::cout << "Не удалось открыть файл" << std::endl;
+    }
+
+    return 0;
+}
---
+++ homework3.4.2/homework3.4.2/homework3.4.2.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+
+class Address {
+private:
+    std::string city;
+    std::string street;
+    int houseNumber;
+    int apartmentNumber;
+public:
+    Address(std::string city, std::string street, int houseNumber, int apartmentNumber) : city(city), street(street), houseNumber(houseNumber), apartmentNumber(apartmentNumber) {}
+
+    std::string getOutputAddress() {
+        return city + ", " + street + ", " + std::to_string(houseNumber) + ", " + std::to_string(apartmentNumber);
+    }
+
+    std::string getCity() {
+        return city;
+    }
+};
+
+void sortAddresses(Address** addresses, int size) {
+    std::sort(addresses, addresses + size, [](Address* a, Address* b) {
+        return a->getCity() < b->getCity();
+        });
+}
+
+int main() {
+    std::ifstream inputFile("in.txt");
+    std::ofstream outputFile("out.txt");
+
+    int n;
+    inputFile >> n;
+
+    Address** addresses = new Address * [n];
+
+    for (int i = 0; i < n; i++) {
+        std::string city, street;
+        int houseNumber, apartmentNumber;
+
+        inputFile.ignore();
+        std::getline(inputFile, city);
+        std::getline(inputFile, street);
+        inputFile >> houseNumber >> apartmentNumber;
+
+        addresses[i] = new Address(city, street, houseNumber, apartmentNumber);
+    }
+
+    sortAddresses(addresses, n);
+
+    outputFile << n << std::endl;
+
+    for (int i = 0; i < n; i++) {
+        outputFile << addresses[i]->getOutputAddress() << std::endl;
+    }
+
+    for (int i = 0; i < n; i++) {
+        delete addresses[i];
+    }
+    delete[] addresses;
+
+    inputFile.close();
+    outputFile.close();
+
+    return 0;
+}
